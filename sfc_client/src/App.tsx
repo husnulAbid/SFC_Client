3a2d19253d386496f9a5d4b0992cab6fcb5c10a0
Husnul Abid <habid96@gmail.com>
2023-10-01 01:57:21 +0200

    react header import

--- sfc_client/src/App.tsx
+++ sfc_client/src/App.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
 import MeatConsumptionCountry from "./components/DataAnalysis/MeatConsumptionCountry";
--- sfc_client/src/components/Dashboard.tsx
+++ sfc_client/src/components/Dashboard.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "../assets/css/MainBody.css";
 
 import Sidebar from "./SideBar";
