3df70e7143fe2a7ac7041e99bef87aabb2e4a0c5
Husnul Abid <habid96@gmail.com>
2023-10-04 02:21:09 +0200

    war effect data in console

--- sfc_client/src/components/DataAnalysis/WarEffect.tsx
+++ sfc_client/src/components/DataAnalysis/WarEffect.tsx
@@ -1,14 +1,41 @@
 import React from "react";
 import Sidebar from "../SideBar";
 
+import "../../assets/css/WarEffect.css"
+
 
 function WarEffect() {
+
+  const handleGetStatButtonClick_1 = async () => {
+    try {
+      const response_1 = await fetch(
+        `http://127.0.0.1:5000/analysis/war_effect_data?metric_name=GDP&year_duration=15`,
+        {
+          method: "GET",
+        }
+      );
+
+      const data_1 = await response_1.json();
+      console.log(data_1)
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <div>
       <Sidebar />
 
       <div className="mainBody">
-        <h5>War Effect</h5>
+        <h5>War Effect on Bangladesh</h5>
+
+
+        <button
+            onClick={handleGetStatButtonClick_1}
+            className="btn btn-primary weButton "
+          >
+            Get Data{" "}
+          </button>
       </div>
     </div>
   );
